94a89c04ebe47f2d075f34177607bf9b5adc0b88
Philhey49 <philippe.heyraud@sfr.fr>
2022-12-14 17:29:39 +0100

    étape 1 1

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,31 +1,33 @@
-const gallery = document.getElementsByClassName('gallery');
-
-const figure = document.createElement('figure');
-
-function res() {
-	let id = document.createElement('figure');
-	let image = document.createElement('img');
-	let title = document.createElement('figcaption');
-	image.src = id.imageUrl;
-	image.alt = id.title;
-	title.innerHTML = `${id.title}`;
-	figure.appendChild(title);
-	figure.appendChild(image);
-	figure.appendChild(id);
-}
-res();
-
-fetch("http://localhost:5678/api/works")	
-.then(function(res) {
-if (res.ok) {
-return res.json();
-}
-}),
+fetch('http://localhost:5678/api/works')	
+.then(function(res) 
 {
-method: "GET",
-headers: { 
-'Accept': 'application/json', 
-}}
+	if (res.ok) 
+	{
+	return res.json();
+	}
+})
+.then(function(projects) 
+{
+	projects.forEach(project => 
+	{
+		const gallery = document.getElementById('test');
+		let figure = document.createElement('figure');
+		let img = document.createElement('img');
+		let figcaption = document.createElement('figcaption');
+		img.src = project.imageUrl;
+		img.alt = project.title;
+		figcaption.innerHTML = `${project.title}`;
+		gallery.appendChild(figure);
+		figure.appendChild(img);
+		figure.appendChild(figcaption);
+	});
+});
+
+
+
+
+
+
 
 
 
