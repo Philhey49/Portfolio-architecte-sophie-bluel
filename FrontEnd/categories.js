8317fd7530d9fc67d9fe5eb0d4bd2a7c8a2d0dd2
Philhey49 <philippe.heyraud@sfr.fr>
2022-12-19 16:00:47 +0100

    début catégories

---
+++ FrontEnd/categories.js
@@ -0,0 +1,15 @@
+fetch('http://localhost:5678/api/categories')	
+.then(function(res) 
+{
+	if (res.ok) 
+	{
+	return res.json();
+	}
+})
+    const categoryId = new Set([
+        ["id:", 1, 2, 3],
+        ["name:", "Objets", "Appartements", "Hotels & restaurants"],
+    ]);
+
+
+    
--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -14,13 +14,17 @@ fetch('http://localhost:5678/api/works')
 		let figure = document.createElement('figure');
 		let img = document.createElement('img');
 		let figcaption = document.createElement('figcaption');
+		let category = document.createElement('category')
 		img.src = project.imageUrl;
 		img.setAttribute("crossorigin", "anonymous");
 		img.alt = project.title;
 		figcaption.innerHTML = `${project.title}`;
+		category.setAttribute("id", 1);
+		category.setAttribute("name", "Objets");
 		gallery.appendChild(figure);
 		figure.appendChild(img);
 		figure.appendChild(figcaption);
+		figure.appendChild(category);
 	});
 });
 
