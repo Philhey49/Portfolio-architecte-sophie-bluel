5dc93ca9a66d7167b41e809ae429b214b5f8e0e1
Philhey49 <philippe.heyraud@sfr.fr>
2023-03-28 15:45:02 +0200

    fin 3

--- FrontEnd/js/works.js
+++ FrontEnd/js/works.js
@@ -2,70 +2,62 @@
 const token = localStorage.getItem('access_token') ? localStorage.getItem('access_token') : false
 const userId = localStorage.getItem('userId') ? localStorage.getItem('userId') : false
 
-
 fetch('http://localhost:5678/api/works')
-.then(function(res) 
-{
-	if (res.ok) 
-	{
-	return res.json();
-	}
-})
-.then(function(projects) 
-{
+.then(response => response.json())
+.then(projects => {
 	
-	projects.forEach(project => 
-	{
-		const gallery = document.getElementById('room');
-		let figure = document.createElement('figure');
-		figure.classList.add('project-item');
-		figure.setAttribute('data-cat', project.categoryId);
-		let img = document.createElement('img');
-		let figcaption = document.createElement('figcaption');
-		img.src = project.imageUrl;
-		img.setAttribute("crossorigin", "anonymous");
-		img.alt = project.title;
-		figcaption.innerHTML = `${project.title}`;
-		gallery.appendChild(figure);
-		figure.appendChild(img);
-		figure.appendChild(figcaption);
+	const gallery = document.querySelector('#room');
+	const modalgallery = document.getElementById('modalroom');
+		
+		projects.forEach(project => {
+			gallery.innerHTML += `
+				<figure class="project-item" data-cat="${project.categoryId}" data-id="${project.id}">
+					<img src="${project.imageUrl}" crossorigin anonymous alt="${project.title}">
+					<figcaption>${project.title}</figcaption>
+				</figure>
+			`
 
 		// S'il est admin, charger les images dans la popup egalement
 		if (token && userId) {
-			const modalgallery = document.getElementById('modalroom');
-			let modalfigure = document.createElement('figure');
-			modalfigure.classList.add('project-item');
-			modalfigure.setAttribute('data-cat', project.categoryId);
-			modalfigure.setAttribute('id', project.id);
-			let modalimg = document.createElement('img');
-			modalimg.classList.add('imggallery');
 
-			let actionOptions = document.createElement('div');
-			actionOptions.classList.add('action-options');
-			actionOptions.innerHTML = `
-				<button class="action-item hover-action-item"><i class="fa-solid fa-arrows-up-down-left-right"></i></button>
-				<button class="action-item delete-item" id=${modalfigure.id}><i class="fa-regular fa-trash-can"></i></button>`;
-			actionOptions.addEventListener('click', (e) => {
-				//let deleteButton = e.target;
-					e.preventDefault()
-					deleteWork(e)
-					//document.getElementById(project.id).remove();
-					  });
-			let modalfigcaption = document.createElement('figcaption');
-			modalimg.src = project.imageUrl;
-			modalimg.setAttribute("crossorigin", "anonymous");
-			modalimg.alt = project.title;
-			modalfigcaption.innerHTML = `éditer`;
-			modalgallery.appendChild(modalfigure);
-			modalfigure.appendChild(actionOptions);
-			modalfigure.appendChild(modalimg);
-			modalfigure.appendChild(modalfigcaption);
+			modalgallery.innerHTML += `
+				<figure class="project-item" data-cat="${project.categoryId}" id="${project.id}">
+					<div class="action-options">
+						<button class="action-item hover-action-item"><i class="fa-solid fa-arrows-up-down-left-right"></i></button>
+						<button class="action-item delete-item" data-id=${project.id}><i class="fa-regular fa-trash-can"></i></button>
+					</div>
+					<img class="imggallery" src="${project.imageUrl}" crossorigin anonymous alt="${project.title}" />
+					<figcaption>éditer</figcaption>
+				</figure>
+			`
 
 		}
 	});
 
-})
-;
+	/* Suppression d'un projet */
+	document.querySelectorAll('.delete-item').forEach(deleteButton => {
+		deleteButton.addEventListener('click', () => {
+
+			let idProject = deleteButton.getAttribute('data-id')
+
+			fetch(`http://localhost:5678/api/works/${idProject}`, {
+					method: 'delete',
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${token}`
+					}
+				})
+				.then(() => {
+					deleteButton.closest('figure').remove()
+					document.querySelector(`.project-item[data-id="${deleteButton.getAttribute('data-id')}"]`).remove()
+					document.querySelector('.modal').classList.add('hide-modal')
+				})
+				.catch(err => console.log(err))
+		})
+
+	})
+
+});
 
 fetch('http://localhost:5678/api/categories')	
 .then(function(res) 
@@ -189,34 +181,9 @@ document.querySelectorAll('.modal-wrapper').forEach(function(wrapper) {
 	})
 })
 
-function deleteWork(e) {
-	let idProject = parseInt(e.target.id)
-	let deleteButton = e.target;
-	fetch(`http://localhost:5678/api/works/${idProject}`, {
-			method: 'DELETE',
-			headers: {'Accept': 'application/json',
-				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${token}`
-			}
-		})
-		.then(function (result) {
-			if (result.status === 200) {
-				return result.json()
-			} else {
-				console.log(result.status)
-				throw new Error(result.error)
-			}
-		})
-		.then(data => {
-			console.log(data)
-			deleteButton.closest('figure').remove()
-		})
-		.catch(err => console.log(err))
-}
+/* Au click sur l'ajout d'un projet */
+document.querySelector('#addbutton').addEventListener('click', (e) => {
 
-const addPhoto = document.querySelector('#addbutton');
-addPhoto.addEventListener('click', (e) => {
-	e.preventDefault();
 	const formData = new FormData();
 	const image = document.querySelector('input[type="file"]').files[0];
 	const title = document.querySelector('input[name="title"]').value;
@@ -225,27 +192,80 @@ addPhoto.addEventListener('click', (e) => {
 	formData.append('image', image);
 	formData.append('title', title);
 	formData.append('category', category);
-	
-	fetch('http://localhost:5678/api/works/',{
-		method: 'POST',
-		headers: { 'Accept': 'application/json', 'Authorization': `Bearer ${token}`},
-		body: formData
-	})
-	.then(function(res) 
-	{
-		if (res.ok) 
-		{
-			return res.json();
-		}
-	})
-	.then(data => {
-		alert("Projet enregistré");
-		console.log(data)
-		gallery.closest('figure').add()
-	})
-	.catch(err => console.log(err))
+	//const addmodalphoto = document.querySelector('.addmodalphoto');
+	//addmodalphoto.innerHTML = '';
+
+	console.log(formData.get('image'), formData.get('title'), formData.get('category'))
+
+	if(
+		formData.get('image') === 'undefined' ||
+		formData.get('title') === '' ||
+		formData.get('category') === '0'
+	) {
+		alert('Veuillez compléter correctement les champs demandé')
+	} else {
+		fetch('http://localhost:5678/api/works/',{
+			method: 'POST',
+			headers: { 'Accept': 'application/json', 'Authorization': `Bearer ${token}`},
+			body: formData
+		})
+		.then(res => res.json())
+		.then(data => {
+
+			// Fermeture de la modale
+			document.querySelectorAll('.modal').forEach(item => {
+				item.classList.add("hide-modal")
+			})
+
+			// Ajout du projet dans la grille de projet
+			document.querySelector('#room').innerHTML += `
+				<figure class="project-item" data-cat="${data.categoryId}" data-id="${data.id}">
+					<img src="${data.imageUrl}" crossorigin anonymous alt="${data.title}">
+					<figcaption>${data.title}</figcaption>
+				</figure>
+			`
+
+			// Ajout du projet dans la grille de la modal
+			document.getElementById('modalroom').innerHTML += `
+				<figure class="project-item" data-cat="${data.categoryId}" id="${data.id}">
+					<div class="action-options">
+						<button class="action-item hover-action-item"><i class="fa-solid fa-arrows-up-down-left-right"></i></button>
+						<button class="action-item delete-item" data-id=${data.id}><i class="fa-regular fa-trash-can"></i></button>
+					</div>
+					<img class="imggallery" src="${data.imageUrl}" crossorigin anonymous alt="${data.title}" />
+					<figcaption>éditer</figcaption>
+				</figure>
+			`
+		})
+		.catch(err => console.log(err))
+	}
 });
 
+// On empeche le formulaire d'ajout de se soumettre
+document.querySelector('#addWorkForm').addEventListener('submit', (e) => e.preventDefault())
+
+// On controle les champs du formulaire
+document.querySelectorAll('#addWorkForm input').forEach(item => {
+	item.addEventListener('change', () => validateAddForm())
+})
+document.querySelector('#addWorkForm select').addEventListener('change', () => validateAddForm())
+
+function validateAddForm() {
+	const image = document.querySelector('input[type="file"]').files[0];
+	const title = document.querySelector('input[name="title"]').value;
+	const category = document.querySelector('#popup-add-category').value;
+
+	if(
+		image !== 'undefined' &&
+		title !== '' &&
+		category !== '0'
+	) {
+		// On change la couleur du bouton
+		document.querySelector('#addbutton').classList.add('validateAddButton')
+	} else {
+		document.querySelector('#addbutton').classList.remove('validateAddButton')
+	}
+}
 const logout = document.querySelector('ul li:nth-child(4)')
 logout.addEventListener('click', function() {
 	localStorage.clear('access_token')
@@ -258,8 +278,7 @@ logout.addEventListener('click', function() {
 	document.querySelectorAll('.not-hide').forEach(item => {
 		item.classList.remove('hide-admin')	
 	});
-})
-  
+}) 
 
 // ECOUTE DU CHANGEMENT DE L'IMAGE
 let addImageInput = document.querySelector('#add-photo-input')
@@ -273,6 +292,8 @@ addImageInput.addEventListener('change', () => {
 		item.hidden = "true"
 	})
 })
+
+
 
 
 
